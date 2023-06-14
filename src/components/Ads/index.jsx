b71ef139210d14aecaddd71a05c6a62ed6da1e6f
BugalloF <bugallof@gmail.com>
2023-06-14 13:48:51 -0300

    Mobile ad

--- src/components/Ads/index.jsx
+++ src/components/Ads/index.jsx
@@ -4,20 +4,34 @@ function Ads ({expandAd,toggleAd}){
     return(
         <div>
             {
-          expandAd['Ad_1']?
+          expandAd['Ad_1'] ?
+            window.innerWidth>600 ?
             <div className="bottom-ad1">
               <button onClick={()=>toggleAd('Ad_1')}> X </button>
               <a href="https://www.exoclick.com/signup/?login=bugallof">
                 <img src="https://www.exoclick.com/banners/468x60.gif" alt='Ads' border="0"/>
               </a>
             </div>
+            :
+            <div className="bottom-ad1-mobile">
+            <button onClick={()=>toggleAd('Ad_1')}> X </button>
+            <a href="https://www.exoclick.com/signup/?login=bugallof"><img src="https://www.exoclick.com/banners/80x30.gif" border="0"/></a>
+          </div>
         :
+        window.innerWidth>600 ?
         <div className="bottom-ad1-hide">
               <button onClick={()=>toggleAd('Ad_1')}> X </button>
               <a href="https://www.exoclick.com/signup/?login=bugallof">
                 <img src="https://www.exoclick.com/banners/468x60.gif" alt='Ads' border="0"/>
               </a>
         </div>
+        :
+        <div className="bottom-ad1-mobile-hide">
+        <button onClick={()=>toggleAd('Ad_1')}> X </button>
+        <a href="https://www.exoclick.com/signup/?login=bugallof">
+          <img src="https://www.exoclick.com/banners/80x30.gif" alt='Ads' border="0"/>
+        </a>
+  </div>
         }
         </div>
     )
